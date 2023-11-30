c5427273bf7a554837951ab155f499ababaf0dc9
Iarek Kovtunenko <iarek.kovtunenko@gmail.com>
2023-11-30 10:40:40 +0000

    solution - final version

--- lib/calculator.ex
+++ lib/calculator.ex
@@ -1,3 +1,5 @@
+# Solution for https://leetcode.com/problems/evaluate-reverse-polish-notation/
+
 defmodule Calculator do
   @spec eval_rpn(tokens :: [String.t()]) :: integer
   def eval_rpn(tokens) do
@@ -10,16 +12,22 @@ defmodule Calculator do
         ) ::
           Integer.t()
 
+  # processing an operation:
+  # take 2 from the stack, apply operation
+  # place the result back on the stack
   def eval_rpn_internal([op | rest_tokens], [a, b | rest_stack])
       when op in ["+", "-", "*", "/"] do
-    eval_rpn_internal(rest_tokens, [apply_op(op, a, b) | rest_stack])
+    eval_rpn_internal(rest_tokens, [apply_op(op, b, a) | rest_stack])
   end
 
+  # keep putting integers on the stack
   def eval_rpn_internal([token | rest_tokens], stack) do
+    # no error handling for invalid numbers
     {int_val, ""} = Integer.parse(token)
     eval_rpn_internal(rest_tokens, [int_val | stack])
   end
 
+  # input empty, single element on the stack = result
   def eval_rpn_internal([], [current_result]) do
     current_result
   end
@@ -27,5 +35,6 @@ defmodule Calculator do
   def apply_op("+", a, b), do: a + b
   def apply_op("-", a, b), do: a - b
   def apply_op("*", a, b), do: a * b
-  def apply_op("/", a, b), do: a / b
+  # forcing the integer division, as `/` returns a float
+  def apply_op("/", a, b), do: div(a, b)
 end
--- test/calculator_test.exs
+++ test/calculator_test.exs
@@ -12,4 +12,31 @@ defmodule CalculatorTest do
   test "repeated addition works in different order" do
     assert Calculator.eval_rpn(["1", "2", "10", "+", "+"]) == 13
   end
+
+  test "subtraction works" do
+    assert Calculator.eval_rpn(["12", "5", "-"]) == 7
+  end
+
+  test "apply division rounds towards 0" do
+    assert Calculator.apply_op("/", 1, 10) == 0
+    assert Calculator.apply_op("/", 1, -10) == 0
+  end
+
+  test "complext expression" do
+    assert Calculator.eval_rpn([
+             "10",
+             "6",
+             "9",
+             "3",
+             "+",
+             "-11",
+             "*",
+             "/",
+             "*",
+             "17",
+             "+",
+             "5",
+             "+"
+           ]) == 22
+  end
 end
